b34f603c3c27be01f5c085c17a29259376da4083
Mark Matienzo <mark@matienzo.org>
2007-12-27 23:32:15 +0000

    template fix; work on formatting etc for output and better query interface
    git-svn-id: http://svn.matienzo.org/public/python/zgw@376 9bbe1849-123c-0410-9313-83ec163a5258

--- zgw.py
+++ zgw.py
@@ -1,4 +1,4 @@
-import re, web
+import re, web, os
 from PyZ3950 import zoom
 from pymarc import marc8_to_unicode
 
@@ -6,28 +6,28 @@ urls = (
   '/search/(\S*)', 'search'
 )
 
-render = web.template.render('search.html')
+render = web.template.render('/home/matienzo/Desktop/python/zgw/templates')
 
 def run_query(c, qs):
   out = []
   query = zoom.Query('CCL', qs)
   results = c.search(query)
   for result in results:
-    out.append(marc8_to_unicode(result.__str__()))
+    out.append(marc8_to_unicode(result.__str__().replace("\n","<br/>")))
   return out
 
 class search:
   def GET(self, query_string):
     zconn = zoom.Connection('z3950.loc.gov', 7090, databaseName='VOYAGER')
     results = run_query(zconn, query_string)
-    print render(query_string=query_string, results=results)
+    print render.search(query_string=query_string, results=results)
     zconn.close()
   
   def POST(self):
     zconn = zoom.Connection('z3950.loc.gov', 7090, databaseName='VOYAGER')
     query_string = web.input()
     results = run_query(zconn, query_string)
-    print render(query_string=query_string, results=results)
+    print render.search(query_string=query_string, results=results)
     zconn.close()
 
 web.webapi.internalerror = web.debugerror
