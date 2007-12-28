6a17e5bec7141182d92da1f4407a25205d92013f
Mark Matienzo <mark@matienzo.org>
2007-12-28 23:44:41 +0000

    added z39.50 uri parser (see rfc 2056). not completely implemented. also, added exeception handler for parser. added 'base' template. will this speed up rendering???
    git-svn-id: http://svn.matienzo.org/public/python/zgw@385 9bbe1849-123c-0410-9313-83ec163a5258

--- parsers.py
+++ parsers.py
@@ -1,6 +1,18 @@
 from web.net import htmlquote
 from pymarc import MARCReader, marc8_to_unicode
 
+class ParseError(Exception):
+  """Base class for exceptions from this module"""
+  pass
+
+class URIParseError(ParseError):
+  def __init__(self, _reason):
+    self.reason = _reason
+    ParseError.__init__(self)
+    
+  def __str__(self):
+    return self.reason
+
 class Parser:
   """Base class for parsers used by zgw"""
   
@@ -14,6 +26,31 @@ class Parser:
     h = htmlquote(h)
     h = h.replace('\n', '<br/>\n')
     return marc8_to_unicode(h)
+  
+  def parse_uri(self, uri):
+    """Parse a Z39.50 URL in RFC 2056 format - not completely implemented"""
+    def pull(v, q, index):
+      _i = index + 1, v.find(q, index)
+      return v[_i[0]:_i[1]], _i[1]
+    
+    if uri[0:10] == 'z39.50s://':
+      host, _index = pull(uri, ':', 9)
+      port, _index = pull(uri, '/', _index)
+      if uri.find('?') == -1:
+        db = uri[_index + 1:]
+        query = None
+      else:
+        db, _index = pull(uri, '?', _index)
+        query = uri[_index + 1:]
+        if uri.find('&') <> -1:
+          #to be implemented...
+          pass
+      return {'host': host, 'port': int(port), 'db': db, 'query': query}
+    elif uri[0:10] == 'z39.50r://':
+      raise URIParseError('URI scheme "z39.50r:" not yet implemented')
+    else:
+      raise URIParseError('%s is not a valid Z39.50 URI' % uri)
+    
   
   def pymarc_serialize(self):
     """Serializes a PyZ3950.zoom.ResultSet object as a pymarc.MARCReader object"""
--- zgw.py
+++ zgw.py
@@ -1,39 +1,50 @@
 import pymarc, web
 from PyZ3950 import zoom
-from parsers import Parser
+from parsers import Parser, ParseError
+
+server_uri = 'z39.50s://z3950.loc.gov:7090/VOYAGER'
 
 urls = (
   '/', 'usage',
-  '/search/(.*)', 'search'
+  '/search/(.*)', 'search',
+  '/parse/(.*)', 'parse'
 )
 
 render = web.template.render('/home/matienzo/Desktop/python/zgw/templates')
 zoom.ResultSet.__bases__ += (Parser,)
 pymarc.Field.__bases__ += (Parser,)
 pymarc.Record.__bases__ += (Parser,)
+server = Parser().parse_uri(server_uri)
 
-def run_query(c, qs):
+def run_query(server, qs):
+  conn = zoom.Connection(server['host'], server['port'], databaseName=server['db'])
   out = []
   query = zoom.Query('CCL', qs)
-  result_set = c.search(query)
+  result_set = conn.search(query)
   reader = result_set.pymarc_serialize()
+  conn.close()
   for result in reader:
     out.append(result.humanize())
   return out
 
+class parse:
+  def GET(self, uri):
+    print Parser().parser.parse_uri(uri)
+    
+  def POST(self):
+    print Parser().parser.parse_uri(web.input())
+
 class search:
   def GET(self, query_string):
-    zconn = zoom.Connection('z3950.loc.gov', 7090, databaseName='VOYAGER')
-    results = run_query(zconn, query_string)
+    print render.base(server=server['host'], query_string=query_string)
+    results = run_query(server, query_string)
     print render.search(query_string=query_string, results=results, total=len(results))
-    zconn.close()
   
   def POST(self):
-    zconn = zoom.Connection('z3950.loc.gov', 7090, databaseName='VOYAGER')
     query_string = web.input()
-    results = run_query(zconn, query_string)
+    print render.base(server=server['host'], query_string=query_string)
+    results = run_query(server, query_string)
     print render.search(query_string=query_string, results=results, total=len(results))
-    zconn.close()
 
 class usage:
   def GET(self):
