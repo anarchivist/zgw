1005fb0ee5ff88e75fd06c232c415c92ca0f4bc8
Mark Matienzo <mark@matienzo.org>
2007-12-29 17:39:59 +0000

    - changed zgw.parse to zgw.validate - removed parsing of uri from zgw.py and replaced with dict containing server info - changing usage template to reference validation class - added docstrings here & there - commented out POST methods as they aren't actually implemented yet
    git-svn-id: http://svn.matienzo.org/public/python/zgw@386 9bbe1849-123c-0410-9313-83ec163a5258

--- zgw.py
+++ zgw.py
@@ -1,22 +1,30 @@
 import pymarc, web
 from PyZ3950 import zoom
-from parsers import Parser, ParseError
-
-server_uri = 'z39.50s://z3950.loc.gov:7090/VOYAGER'
+from parsers import Parser
 
 urls = (
   '/', 'usage',
   '/search/(.*)', 'search',
-  '/parse/(.*)', 'parse'
+  '/validate/(.*)', 'validate'
 )
+
+server = {
+  'host': 'z3950.loc.gov',
+  'port': 7090,
+  'db': 'VOYAGER'
+}
+
+#commented out as this is just an example and not yet totally implemented
+#server_uri = 'z39.50s://z3950.loc.gov:7090/VOYAGER'
+#server = Parser().parse_uri(server_uri)
 
 render = web.template.render('/home/matienzo/Desktop/python/zgw/templates')
 zoom.ResultSet.__bases__ += (Parser,)
 pymarc.Field.__bases__ += (Parser,)
 pymarc.Record.__bases__ += (Parser,)
-server = Parser().parse_uri(server_uri)
 
 def run_query(server, qs):
+  """Creates Z39.50 connection, sends query, serializes and humanizes results"""
   conn = zoom.Connection(server['host'], server['port'], databaseName=server['db'])
   out = []
   query = zoom.Query('CCL', qs)
@@ -27,28 +35,32 @@ def run_query(server, qs):
     out.append(result.humanize())
   return out
 
-class parse:
-  def GET(self, uri):
-    print Parser().parser.parse_uri(uri)
-    
-  def POST(self):
-    print Parser().parser.parse_uri(web.input())
-
 class search:
+  """web.py class for submitting a Z39.50 query and returning results"""
   def GET(self, query_string):
     print render.base(server=server['host'], query_string=query_string)
     results = run_query(server, query_string)
     print render.search(query_string=query_string, results=results, total=len(results))
   
-  def POST(self):
-    query_string = web.input()
-    print render.base(server=server['host'], query_string=query_string)
-    results = run_query(server, query_string)
-    print render.search(query_string=query_string, results=results, total=len(results))
+  #def POST(self):
+    #query_string = web.input()
+    #print render.base(server=server['host'], query_string=query_string)
+    #results = run_query(server, query_string)
+    #print render.search(query_string=query_string, results=results, total=len(results))
 
 class usage:
+  """web.py class to display usage information"""
   def GET(self):
     print render.usage()
+    
+class validate:
+  """web.py class to validate Z39.50 URIs"""
+  def GET(self, uri):
+    print Parser().parse_uri(uri)
+    
+  #def POST(self):
+    #print Parser().parse_uri(web.input())
+
 
 web.webapi.internalerror = web.debugerror
 if __name__ == '__main__': web.run(urls, globals())
